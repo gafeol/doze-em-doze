d6ecd7aad5a1be0f0543ac00297d4c949db8739e
gafeol <gabfeol@gmail.com>
2020-12-07 20:52:55 -0300

    Checking name value on edittext

--- app/src/main/java/com/gafeol/dozeemdoze/AddMedication.kt
+++ app/src/main/java/com/gafeol/dozeemdoze/AddMedication.kt
@@ -15,12 +15,23 @@ class AddMedication : AppCompatActivity() {
 
     private val userUID = FirebaseAuth.getInstance().currentUser?.uid.toString()
 
-    fun saveMedication(view: View) {
+    private fun checkForm() : Boolean {
         var nameEditText = findViewById<EditText>(R.id.nameEditText)
-        val db = FirebaseDatabase.getInstance()
-        val medName = nameEditText.text.toString()
-        val myRef = db.getReference("$userUID/medication/$medName")
-        myRef.setValue(true).addOnSuccessListener { finish() }
-        myRef.child("img").setValue(R.drawable.ic_pills)
+        if(nameEditText.text.isEmpty()){
+            nameEditText.setError("Digite o nome da medicação")
+            return false
+        }
+        return true
+    }
+
+    fun saveMedication(view: View) {
+        if(checkForm()){
+            var nameEditText = findViewById<EditText>(R.id.nameEditText)
+            val db = FirebaseDatabase.getInstance()
+            val medName = nameEditText.text.toString()
+            val myRef = db.getReference("$userUID/medication/$medName")
+            myRef.setValue(true).addOnSuccessListener { finish() }
+            myRef.child("img").setValue(R.drawable.ic_pills)
+        }
     }
 }
