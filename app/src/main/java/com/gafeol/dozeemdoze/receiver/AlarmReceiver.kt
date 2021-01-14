f9849fa872e292c2b366c9e174991455622729fd
gafeol <gabfeol@gmail.com>
2021-01-14 00:12:00 -0300

    Add time picker
    Also creates a custom time picker with 24h instead of AM/PM

--- app/src/main/java/com/gafeol/dozeemdoze/receiver/AlarmReceiver.kt
+++ app/src/main/java/com/gafeol/dozeemdoze/receiver/AlarmReceiver.kt
@@ -21,7 +21,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.gafeol.dozeemdoze.util.sendNotification
 
@@ -29,7 +28,7 @@ class AlarmReceiver: BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         // TODO: Step 1.10 [Optional] remove toast
-        Toast.makeText(context, "Toma o remedio ai", Toast.LENGTH_SHORT).show()
+        // Toast.makeText(context, "TOMA o remedio ai", Toast.LENGTH_SHORT).show()
         Log.d("ALRM", "Ativou o alarme para tomar o remedio!")
 
         // TODO: Step 1.9 add call to sendNotification
