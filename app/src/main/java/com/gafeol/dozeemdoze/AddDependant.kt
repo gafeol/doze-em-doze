54668d592d007b687c35174b5ba164c5f81d3c94
gafeol <gabfeol@gmail.com>
2021-01-18 22:14:43 -0300

    Add dependant skeleton

---
+++ app/src/main/java/com/gafeol/dozeemdoze/AddDependant.kt
@@ -0,0 +1,11 @@
+package com.gafeol.dozeemdoze
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddDependant : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_dependant)
+    }
+}
