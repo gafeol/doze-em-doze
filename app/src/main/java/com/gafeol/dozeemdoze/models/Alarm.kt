e7c067ea3b42fc752ce3dc51c85a04f4df2aba93
gafeol <gabfeol@gmail.com>
2021-01-25 01:05:27 -0300

    Add alarm model and row style

---
+++ app/src/main/java/com/gafeol/dozeemdoze/models/Alarm.kt
@@ -0,0 +1,51 @@
+package com.gafeol.dozeemdoze.models
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.gafeol.dozeemdoze.R
+import com.gafeol.dozeemdoze.util.formatTime
+import com.google.firebase.database.DataSnapshot
+import kotlinx.android.synthetic.main.row_alarm.view.*
+
+class Alarm(val time: Int,
+                val medications: Array<String>) {
+
+    constructor(snap: DataSnapshot) : this(
+        snap.key!! as Int,
+        snap.children.map { it.key!! }.toTypedArray()
+    )
+
+}
+
+// Adapter of alarmList to an ArrayAdapter
+class AlarmAdapter(context: Context, alarmList: List<Alarm>)
+    : ArrayAdapter<Alarm>(context,  R.layout.row_alarm, alarmList) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val alarm = getItem(position)!!
+        var holder = ViewHolder()
+        val retView: View
+
+        if(convertView == null){
+            val inflater = LayoutInflater.from(context)
+            retView = inflater.inflate(R.layout.row_alarm, parent, false)
+            holder.time = retView.timeTextView
+            holder.meds = retView.medicationsTextView
+            retView.tag = holder
+        } else {
+            retView = convertView
+            holder = retView.tag as ViewHolder
+        }
+        holder.time.text = formatTime(alarm.time)
+        holder.meds.text = alarm.medications.joinToString(", ")
+        return retView
+    }
+
+    internal class ViewHolder {
+        lateinit var time: TextView
+        lateinit var meds: TextView
+    }
+}
