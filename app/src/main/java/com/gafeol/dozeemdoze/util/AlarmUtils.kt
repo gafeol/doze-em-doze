ea41b41d16840ada5687620bda75c1caf782636f
gafeol <gabfeol@gmail.com>
2021-03-13 21:58:05 -0300

    Change alarm setup from ELAPSED TIME to Calendar-based
    This change greatly improves precision of alarms

--- app/src/main/java/com/gafeol/dozeemdoze/util/AlarmUtils.kt
+++ app/src/main/java/com/gafeol/dozeemdoze/util/AlarmUtils.kt
@@ -4,7 +4,6 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.SystemClock
 import android.util.Log
 import com.gafeol.dozeemdoze.receiver.AlarmReceiver
 import java.util.*
@@ -12,9 +11,6 @@ import java.util.*
 fun minutesToday(c: Calendar = Calendar.getInstance()) = c.get(Calendar.HOUR_OF_DAY)*60 + c.get(Calendar.MINUTE)
 
 fun setAlarm(context: Context, intent: Intent, time : Int){
-    var timeNow = SystemClock.elapsedRealtime()
-    timeNow = (timeNow/60000)*60000 // Removing seconds from timeNow
-    val minutesToday = minutesToday()
     val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
     val pendingIntent = PendingIntent.getService(context, time, intent, PendingIntent.FLAG_UPDATE_CURRENT)
     if (pendingIntent != null && alarmManager != null) {
@@ -26,12 +22,21 @@ fun setAlarm(context: Context, intent: Intent, time : Int){
         PendingIntent.getBroadcast(context, time, alarmIntent, 0)
     }
 
-    val alarmAt = timeNow + ((24*60 + time - minutesToday)%(24*60))*60*1000L
+    val alarmCalendar = GregorianCalendar().also{
+        it.timeInMillis = System.currentTimeMillis()
+        it.set(Calendar.HOUR_OF_DAY, time/60)
+        it.set(Calendar.MINUTE, time%60)
+        it.set(Calendar.SECOND, 0)
+        it.set(Calendar.MILLISECOND, 0)
+        if(time < minutesToday()) {
+            it.add(Calendar.DAY_OF_MONTH, 1)
+        }
+    }
     alarmManager?.setRepeating(
-            AlarmManager.ELAPSED_REALTIME_WAKEUP,
-            alarmAt,
+            AlarmManager.RTC_WAKEUP,
+            alarmCalendar.timeInMillis,
             AlarmManager.INTERVAL_DAY,
             alarmIntent
     )
-    Log.d("ALARM", "Set alarm for $time triggering at $alarmAt")
+    Log.d("ALARM", "Set alarm RTC WAKEUP for timeinMilis ${alarmCalendar.timeInMillis}")
 }
